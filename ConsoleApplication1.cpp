3fa14e26192ff27b6d0f39462d5ae72915149cc0
XZB20041122 <102888138+XZB20041122@users.noreply.github.com>
2023-10-09 22:19:34 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <stdio.h>/*编译预处理指令*/
+
+int main()/*主函数的函数头*/
+{
+	printf("这是我第一个代码.\n");/*输出要显示的字符串*/
+	return(0);/*程序返回值0*/
+   
+}
+echo "# TESTE1" >> README.md
+git init
+git add README.md
+git commit - m “第一次提交”
+git 分支 - M main
+git 添加远程原点https ://github.com/XZB20041122/TESTE1.git
+git Push - u 起源 主要
